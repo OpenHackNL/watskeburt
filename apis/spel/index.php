bef0a33b115c112d6c1151af17097737e03f717e
Johan Groenen <johan.groenen+github@gmail.com>
2015-07-04 13:43:40 +0000

    vragen done

---
+++ apis/spel/index.php
@@ -0,0 +1,10 @@
+<?php
+
+echo json_encode([
+    "date" => "4 juli",
+    "year" => "1831",
+    "event" => "James Monroe (73), vijfde president van de Verenigde Staten",
+    "eventType" => "Overleden",
+    "answers" => [],
+    "round" => 1
+]);
--- apis/vragen/index.php
+++ apis/vragen/index.php
@@ -1,6 +1,23 @@
 <?php
 
-$url = "https://nl.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&explaintext=&titles=30_juni&exsectionformat=plain";
+$day = date("j");
+$month = [
+    1 => "januari",
+    "februari",
+    "maart",
+    "april",
+    "mei",
+    "juni",
+    "juli",
+    "augustus",
+    "september",
+    "oktober",
+    "november",
+    "december"
+][date("n")];
+
+$url = "https://nl.wikipedia.org/w/api.php?format=json&action=query" .
+       "&prop=extracts&explaintext=&titles={$day}_{$month}&exsectionformat=wiki";
 
 $raw = file_get_contents($url);
 
@@ -10,30 +27,25 @@ $page = reset($json->query->pages);
 
 $lines = explode("\n", $page->extract);
 
-$questions = [];
+$events = [];
 
 $eventType = "";
 
 foreach ($lines as $line) {
-    if (in_array($line, ["Geboren", "Overleden"])) {
-        $eventType = $line;
-    } else if ($line === "") {
-        
-    }
-    if (preg_match("/^[0-9]{4} -/", $line)) {
-        $question = $line;
-        if ($eventType) {
-            $question .= ", " . strtolower($eventType);
-        }
-        $questions[] = $question;
+    if (preg_match("/^== [a-zA-Z]* ==$/", $line)) {
+        $eventType = trim(str_replace("==", "", $line));
+    } else if (preg_match("/^[0-9]{4} -/", $line)) {
+        $events[] = $line . " - " . $eventType;
     }
 }
 
-$question = $questions[array_rand($questions)];
+$event = $events[array_rand($events)];
 
-list($year, $question) = explode(" - ", $question);
+list($year, $event, $eventType) = explode(" - ", $event);
 
 echo json_encode([
+    "date" => $day . " " . $month,
     "year" => $year,
-    "event" => $question
+    "event" => $event,
+    "eventType" => $eventType,
 ]);
